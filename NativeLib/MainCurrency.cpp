1a499f8983201c290d98797e784486ed4f64aa9f
berun_ak20_@nuwm.edu.ua <berun_ak20_@nuwm.edu.ua>
2021-06-29 11:36:21 +0300

    added main counter and DataLoader

---
+++ NativeLib/MainCurrency.cpp
@@ -0,0 +1,49 @@
+#include "MainCurrency.h"
+
+void MainCurrency::_register_methods()
+{
+
+}
+
+void MainCurrency::_init()
+{
+	CurrentValue = DataLoader::GetSingleton()-> ReturnMainCurrency();
+
+	AddValue(11);
+	CounterUpdate();
+
+}
+
+void MainCurrency::CounterUpdate()
+{
+	String str = String::num(CurrentValue);
+	set_text(str);
+}
+
+void MainCurrency::AddValue(int Value)
+{
+	CurrentValue += Value;
+
+	DataLoader::GetSingleton()->UpdateMainCurrency(CurrentValue);
+
+	CounterUpdate();
+}
+
+bool MainCurrency::SubtractValue(int Value)
+{
+	if (CurrentValue - Value < 0)
+		return false;
+
+	CurrentValue -= Value;
+
+	CounterUpdate();
+
+	DataLoader::GetSingleton()->UpdateMainCurrency(CurrentValue);
+
+	return true;
+}
+
+int MainCurrency::ReturnValue()
+{
+	return CurrentValue;
+}
---
+++ NativeLib/MainCurrency.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <Label.hpp>
+#include "CommonLib.h"
+#include "DataLoader.h"
+
+class MainCurrency :
+	public Label
+{
+	GODOT_CLASS(MainCurrency, Label);
+
+public:
+	static void _register_methods();
+	void _init();
+
+	void AddValue(int Value);
+	bool SubtractValue(int Value);
+
+	int ReturnValue();
+
+private:
+	void CounterUpdate();
+
+	int CurrentValue = 0;
+};
+
+
